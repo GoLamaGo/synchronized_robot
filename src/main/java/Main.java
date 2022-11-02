0d5b5b1d04a796ea8c6f855c76c10b737dda9e3a
Andrew <aka.slurm@gmail.com>
2022-11-02 14:41:38 +0600

    created Runnable variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,42 +3,39 @@ import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final Map<Integer, Integer> sizeToFreq = new TreeMap<>();     // TreeMap для сортировки по key
 
     public static void main(String[] args) {
         int amountOfThreads = 1000;
         String letters = "RLRFR";
         int stringLength = 100;
         char charToFind = 'R';
-
         var pool = Executors.newFixedThreadPool(amountOfThreads);
 
+        Runnable runnable = () -> {
+            String route = generateRoute(letters, stringLength);
+            int amountOfR = 0;
+            for (int i = 0; i < route.length(); i++) {
+                if (route.charAt(i) == charToFind) {
+                    amountOfR++;
+                }
+            }
+            synchronized (sizeToFreq) {
+                if (sizeToFreq.containsKey(amountOfR)) {
+                    sizeToFreq.put(amountOfR, sizeToFreq.get(amountOfR) + 1);
+                } else {
+                    sizeToFreq.put(amountOfR, 1);
+                }
+            }
+        };
+
         for (int t = 0; t < amountOfThreads; t++) {
-            pool.submit(
-                    new Thread(() -> {
-                        String route = generateRoute(letters, stringLength);
-                        int amountOfR = 0;
-                        for (int i = 0; i < route.length(); i++) {
-                            if (route.charAt(i) == charToFind) {
-                                amountOfR++;
-                            }
-                        }
-                        synchronized (sizeToFreq) {
-                            if (sizeToFreq.containsKey(amountOfR)) {
-                                sizeToFreq.put(amountOfR, sizeToFreq.get(amountOfR) + 1);
-                            } else {
-                                sizeToFreq.put(amountOfR, 1);
-                            }
-                        }
-                    }
-                    )
-            );
+            pool.submit(new Thread(runnable));
         }
 
         pool.shutdown();
 
         sizeToFreq.forEach((x, y) -> System.out.println(x + " (" + y + " раз)"));
-
     }
 
     public static String generateRoute(String letters, int length) {
